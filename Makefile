979e95393c521ca759c5be7461a578ff35c1d927
HazardHaide <nladonov@gmail.com>
2024-08-06 14:34:46 +0300

    STEP 3

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 install:
 	poetry install
+	
 lint:
 	poetry run flake8 brain_games
 
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,11 @@
+from brain_games.cli import welcome_user
+
+name = None
+
 def main():
-    print("Welcome to the Brain Games!")
+    global user_name
+    name = welcome_user()
+
 
 if __name__ == '__main__':
     main()
